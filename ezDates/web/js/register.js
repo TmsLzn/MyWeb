e62d1af4402ab9d27b51349ec6747ed86e03e6b9
tomlozrom <tomlozrom@alum.us.es>
2023-05-19 09:55:05 +0200

    Hecho

--- ezDates/web/js/register.js
+++ ezDates/web/js/register.js
@@ -1,9 +1,32 @@
 import { messageRenderer } from "/js/renderers/messages.js";
 import { userValidator } from "/js/validators/register.js";
+import { sessionManager } from "/js/utils/session.js";
+import { authAPI_auto } from "/js/api/_auth.js";
 function main() {
     let registerForm = document.getElementById("register-form");
     registerForm.onsubmit = handleSubmitRegister;
+    valoresPorDefecto();
 }
+
+function valoresPorDefecto() {
+    let estatura = document.getElementById('estatura-input');
+    estatura.value = 170;
+    let peso = document.getElementById('peso-input');
+    peso.value = 75;
+    let telefono = document.getElementById('phone-input');
+    telefono.value = 75;
+    let genero = document.getElementById('gender-select');
+    genero.value = 'other';
+    let colorOjos = document.getElementById('color-ojos-input');
+    colorOjos.value = 'other';
+    let colorPelo = document.getElementById('color-pelo-input');
+    colorPelo.value = 'other';
+    let bio = document.getElementById('biografia-input');
+    bio.value = 'Hola soy un/a nuev@ usuari@.';
+}
+
+
+
 function handleSubmitRegister(event) {
     event.preventDefault();
     let form = event.target;
@@ -16,7 +39,23 @@ function handleSubmitRegister(event) {
             messageRenderer.showErrorAsAlert(error);
         }
     }
+    else {
+        sendRegister(formData);
+    }
 }
+
+async function sendRegister(formData) {
+    try {
+        let loginData = await authAPI_auto.register(formData);
+        let sessionToken = loginData.sessionToken;
+        let loggedUser = loginData.user;
+        sessionManager.login(sessionToken, loggedUser);
+        window.location.href = "index.html";
+    } catch (err) {
+        messageRenderer.showErrorAsAlert("Error registering a new user", err);
+    }
+}
+
 
 
 
